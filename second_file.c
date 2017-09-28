71b5907a4eb94fd8fcf679a6ec6fb43ace6fe5f6
terehovaanuta <terehovaanuta@gmail.com>
2017-09-28 14:49:11 +0300

    Revert "commit branch"
    This reverts commit 13d10cef4203ff9d6849ec08dc15e343cf8dddd0.
    
    # Conflicts:
    #	second_file.c

--- second_file.c
+++ second_file.c
@@ -6,13 +6,11 @@ int main(int argc, char **argv)
     printf("Hello! My name is Ann\n")
     char first[255], last[255];
 
-    printf("Enter your first name: ");
-    fgets(first, 255, stdin);
-    first[strlen(first)-1] = '\0'; /* remove the newline at the end */
+    printf("Enter your name: ");
+    fgets(name, 255, stdin);
+    printf("length = %d\n", strlen(name)); /* debug line */
+    name[strlen(name)-1] = '\0'; /* remove the newline at the end */
 
-    printf("Now enter your last name: ");
-    gets(last); /* buffer overflow? what's that? */
-
-    printf("Hello %s %s!\n", first, last);
+    printf("Hello %s!\n", name);
     return 0;
 }
